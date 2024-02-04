ea745c15c2c9559085fd8fe4d6defa5cd28659f5
Abhijit Chakraborty <99414748+abhijitchak103@users.noreply.github.com>
2024-02-04 13:15:16 +0000

    Week 2 Homework solution set

--- mage/magic-zoomcamp/data_exporters/to_gcs_partition_parquet.py
+++ mage/magic-zoomcamp/data_exporters/to_gcs_partition_parquet.py
@@ -6,20 +6,20 @@ if 'data_exporter' not in globals():
     from mage_ai.data_preparation.decorators import data_exporter
 
 
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "/home/src/dtc-de-course-411615-43a4f70326b5.json"
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "/home/src/dtc-de-course-411615-aaed8e8043b4.json"
 
 bucket_name = 'mage-abhijit-1'
 project_id = 'dtc-de-course-411615'
 
-table_name = 'nyc_taxi_data'
+table_name = 'mage'
 
 root_path = f"{bucket_name}/{table_name}"
 
-
 @data_exporter
 def export_data(data, *args, **kwargs):
-    data['tpep_pickup_date'] = data['tpep_pickup_datetime'].dt.date
-
+    # lpep_pickup_date column
+    data['lpep_pickup_date'] = data['lpep_pickup_datetime'].dt.date
+    
     table = pa.Table.from_pandas(data)
 
     gcs = pa.fs.GcsFileSystem()
@@ -28,6 +28,6 @@ def export_data(data, *args, **kwargs):
     pq.write_to_dataset(
         table,
         root_path=root_path,
-        partition_cols=['tpep_pickup_date'],
+        partition_cols=['lpep_pickup_date'],
         filesystem=gcs
     )
