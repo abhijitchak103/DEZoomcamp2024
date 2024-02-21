dc3cb848cab1478854089c90d764cb7d3e38d0d6
Abhijit Chakraborty <99414748+abhijitchak103@users.noreply.github.com>
2024-02-21 08:05:15 +0000

    Add code to load data for yellow taxi

---
+++ mage/magic-zoomcamp/data_loaders/load_csv_unmage.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import io
+
+url_base = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/yellow_tripdata_'
+month = '2019-01'
+extension = '.csv.gz'
+
+url = url_base + month + extension
+
+months = ['2019-02', '2019-03', '2019-04', '2019-05', '2019-06', 
+        '2019-07', '2019-08', '2019-09', '2019-10', '2019-11', '2019-12', 
+        '2020-02', '2020-03', '2020-04', '2020-05', '2020-06', 
+        '2020-07', '2020-08', '2020-09', '2020-10', '2020-11', '2020-12',]
+
+taxi_dtypes = {
+    'VendorID': pd.Int64Dtype(),
+    'passenger_count': pd.Int64Dtype(),
+    'trip_distance': float,
+    'RatecodeID': pd.Int64Dtype(),
+    'store_and_fwd_flag': str,
+    'PULocationID': pd.Int64Dtype(),
+    'DOLocationID': pd.Int64Dtype(),
+    'payment_type': pd.Int64Dtype(),
+    'fare_amount': float,
+    'extra': float,
+    'mta_tax': float,
+    'tip_amount': float,
+    'tolls_amount': float,
+    'improvement_surcharge': float,
+    'total_amount': float,
+    'congestion_surcharge': float 
+}
+
+parse_dates = ['tpep_pickup_datetime', 'tpep_dropoff_datetime']
+
+df = pd.read_csv(url, sep=',', compression='gzip', parse_dates=parse_dates)
+
+for month in months:
+    url = url_base + month + extension
+    print(f'Reading from: {url}')
+    df_current = pd.read_csv(url, sep=',', compression='gzip', parse_dates=parse_dates)
+    df = pd.concat([df, df_current], ignore_index=True)
+    print(f'Number of rows after appending {month}: {df.shape[0]}')
+    print('-'*50)
