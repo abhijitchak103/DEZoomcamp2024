32ddc0a7ac4e6d2db501b9b9120b1f8b97b6ddc1
Abhijit Chakraborty <abhichaxz@gmail.com>
2024-02-22 14:32:19 +0000

    add fhv, green, yellow

---
+++ Week_4/Homework/models/staging/stg_fhv_tripdata.sql
@@ -0,0 +1,31 @@
+with 
+
+source as (
+
+    select * from {{ source('staging', 'fhv_tripdata') }}
+
+),
+
+renamed as (
+
+    select
+        dispatching_base_num,
+        pickup_datetime,
+        dropoff_datetime,
+        pulocationid,
+        dolocationid,
+        sr_flag,
+        affiliated_base_number
+
+    from source
+
+)
+
+select * from renamed
+
+-- dbt build --select <model.sql> --vars '{'is_test_run: false}'
+{% if var('is_test_run', default=true) %}
+
+  limit 100
+
+{% endif %}
