39ec3bc726b72f7060bffdee37e8b04ab58dbed5
Abhijit Chakraborty <abhichaxz@gmail.com>
2024-02-22 16:37:46 +0000

    add fact_fhv_trips

--- Week_4/Homework/models/staging/stg_fhv_tripdata.sql
+++ Week_4/Homework/models/staging/stg_fhv_tripdata.sql
@@ -1,31 +1,27 @@
-with 
+{{ config(materialized="view") }}
 
-source as (
+with
+    source as (
 
-    select * from {{ source('staging', 'fhv_tripdata') }}
+        select *
+        from {{ source("staging", "fhv_tripdata") }}
+        where extract(year from pickup_datetime) = 2019
 
-),
+    )
+select
+    {{ dbt_utils.generate_surrogate_key(["dispatching_base_num", "pickup_datetime"]) }}
+    as fhvid,
+    {{ dbt.safe_cast("dispatching_base_num", api.Column.translate_type("string")) }}
+    as dispatching_base_number,
+    cast(pickup_datetime as timestamp) as pickup_datetime,
+    cast(dropoff_datetime as timestamp) as dropoff_datetime,
+    {{ dbt.safe_cast("pulocationid", api.Column.translate_type("integer")) }}
+    as pickup_locationid,
+    {{ dbt.safe_cast("dolocationid", api.Column.translate_type("integer")) }}
+    as dropoff_locationid,
+    cast(sr_flag as numeric) as sr_flag,
+    {{ dbt.safe_cast("affiliated_base_number", api.Column.translate_type("string")) }}
+    as affiliated_base_number
+from source
 
-renamed as (
-
-    select
-        dispatching_base_num,
-        pickup_datetime,
-        dropoff_datetime,
-        pulocationid,
-        dolocationid,
-        sr_flag,
-        affiliated_base_number
-
-    from source
-
-)
-
-select * from renamed
-
--- dbt build --select <model.sql> --vars '{'is_test_run: false}'
-{% if var('is_test_run', default=true) %}
-
-  limit 100
-
-{% endif %}
+{% if var("is_test_run", default=false) %} limit 100 {% endif %}
