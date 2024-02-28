61c1a2d32ce736424d5df32d96e7724400a69a9d
mraabhijit <abhijityachak@gmail.com>
2024-02-28 15:46:53 +0000

    Lecture 5 Module 5

---
+++ Week_5/download_data.sh
@@ -0,0 +1,24 @@
+# To exit bash script in case we get 404
+set -e
+
+# Define variables
+TAXI_TYPE=$1 # $1 signifies the first argument in bash command
+YEAR=$2
+
+URL_PREFIX="https://github.com/DataTalksClub/nyc-tlc-data/releases/download"
+
+for MONTH in {1..12}; do
+    FMONTH=`printf "%02d" ${MONTH}`
+
+    URL="${URL_PREFIX}/${TAXI_TYPE}/${TAXI_TYPE}_tripdata_${YEAR}-${FMONTH}.csv.gz"
+
+    LOCAL_PREFIX="data/raw/${TAXI_TYPE}/${YEAR}/${FMONTH}"
+    LOCAL_FILE="${TAXI_TYPE}_tripdata_${YEAR}_${FMONTH}.csv.gz"
+    LOCAL_PATH="${LOCAL_PREFIX}/${LOCAL_FILE}"
+
+    echo "downloading ${URL} to ${LOCAL_PATH}..."
+    mkdir -p ${LOCAL_PREFIX}
+    wget ${URL} -O ${LOCAL_PATH}
+    echo "...successfully downloaded ${URL} to ${LOCAL_PATH}"
+
+done
